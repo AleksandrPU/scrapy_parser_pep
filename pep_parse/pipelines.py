fec48b613c3cdf3a2ae86b7c382b62385caa97e9
Паутов Александр <sh2d000w@gmail.com>
2024-04-07 17:48:20 +0300

    feat: Add item PepParse. feat: Write spider pep. feat: Add pipeline PepParse. feat: Add files with results. fix: Delete empty files. fix: Delete waste comments.

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,29 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+from collections import defaultdict
+from datetime import datetime as dt
+from pathlib import Path
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+BASE_DIR = Path(__file__).parent.parent
 
 
 class PepParsePipeline:
+    def open_spider(self, spider):
+        self.statuses = defaultdict(int)
+
     def process_item(self, item, spider):
+        self.statuses[item['status']] += 1
+
         return item
+
+    def close_spider(self, spider):
+        path_file = BASE_DIR / 'results'
+        file_name = (
+            f'status_summary_{dt.now().isoformat(timespec="seconds")}.csv')
+
+        with open(path_file / file_name, 'w') as csv_file:
+            writer = csv.writer(csv_file)
+
+            writer.writerow(('Статус', 'Количество'))
+            writer.writerows(self.statuses.items())
+            writer.writerow(('Total', sum(self.statuses.values())))
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,36 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
+
+    def parse_pep(self, response):
+        number, name = response.xpath(
+            '//h1[@class="page-title"]/text()'
+        ).get().split(' – ', 1)
+
+        status = response.xpath(
+            '//dl[contains(@class, "rfc2822")]'
+            '/dt[contains(text(), "Status")]'
+            '/following-sibling::dd/abbr/text()'
+        ).get()
+
+        yield PepParseItem(
+            {
+                'number': int(number.split()[1]),
+                'name': name,
+                'status': status,
+            }
+        )
 
     def parse(self, response):
-        pass
+        peps = response.xpath('//section[@id="numerical-index"]/*/tbody/tr')
+        pep_links = peps.xpath(
+            'td/a[contains(@class, "pep")]/@href').getall()
+
+        for link in pep_links:
+            yield response.follow(link, callback=self.parse_pep)
